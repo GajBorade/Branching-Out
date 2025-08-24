efb1e14b3861a389ebb783a950df744b6ccbc57e
GajBorade <141010355+GajBorade@users.noreply.github.com>
2025-08-24 16:19:03 +0200

    Reformat code to PEP8 standards with Black

--- filter_users.py
+++ filter_users.py
@@ -1,17 +1,18 @@
 import json
 
+
 def filter_users_by_name(name):
     with open("users.json", "r") as file:
         users = json.load(file)
-    
+
     filtered_users = [user for user in users if user["name"].lower() == name.lower()]
-    
+
     for user in filtered_users:
         print(user)
 
 
 def filter_users_by_age(age):
-    with open ("users.json", "r") as file:
+    with open("users.json", "r") as file:
         users = json.load(file)
 
     filtered_users = [user for user in users if user["age"] == age]
@@ -20,7 +21,7 @@ def filter_users_by_age(age):
 
 
 def filter_users_by_email(email):
-    with open ("users.json", "r") as file:
+    with open("users.json", "r") as file:
         users = json.load(file)
 
     filtered_users = [user for user in users if user["email"].lower() == email.lower()]
@@ -29,8 +30,14 @@ def filter_users_by_email(email):
 
 
 if __name__ == "__main__":
-    filter_option = input("What would you like to filter by? (Currently, only name, age and email is supported): ").strip().lower()
-    
+    filter_option = (
+        input(
+            "What would you like to filter by? (Currently, only name, age and email is supported): "
+        )
+        .strip()
+        .lower()
+    )
+
     if filter_option == "name":
         name_to_search = input("Enter a name to filter users: ").strip()
         filter_users_by_name(name_to_search)
